197e091c2aba109d2bccc0828976d816bb5dee15
grbuguj <bugs10613@gmail.com>
2026-09-09 09:29:55 +0900

    feat: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/finger/fingerjourneybackend/FingerJourneyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finger.fingerjourneybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerJourneyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingerJourneyBackendApplication.class, args);
+    }
+
+}
