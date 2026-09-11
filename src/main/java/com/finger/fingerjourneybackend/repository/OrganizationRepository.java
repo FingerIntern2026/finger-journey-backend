81b2ee405827fea34016084b46a5cf94acc9f0e6
h0_0ngji <jyr9ysdhhz@privaterelay.appleid.com>
2026-09-11 10:09:42 +0900

    Revert "feat: 사원 등록/수정 컨트롤러 구현 (#13)" (#14)
    This reverts commit 304523b40c9783edfe62eb6801a11a8cab6cb8af.

--- src/main/java/com/finger/fingerjourneybackend/repository/OrganizationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finger.fingerjourneybackend.repository;
-
-import com.finger.fingerjourneybackend.entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-}
--- src/main/java/com/finger/fingerjourneybackend/repository/PositionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finger.fingerjourneybackend.repository;
-
-import com.finger.fingerjourneybackend.entity.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Long>{
-}
